1d745f8c3e1208d02b6c9e93b84588c366b15a43
EC2 Default User <kanumala@vols.utk.edu>
2022-05-13 17:44:06 +0000

    updated

--- Test_Python_Jenkins_Pipeline.py
+++ Test_Python_Jenkins_Pipeline.py
@@ -2,5 +2,5 @@ print(" Hello Python Pipeline")
 print(" Hello Jenkins Pipeline")
 print(" Hello AWS Pipeline")
 print(" Hello Python + Jenkins + AWS Pipeline")
-print(" Success")
+print(" Git push even Success")
 #comments
